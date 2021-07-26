23e201300c20c650eb3e16fe5d72836f0d1b37e4
JuanMoli111 <hombrebota@hotmail.com>
2021-07-26 18:32:09 -0300

    bug fixes

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-from funciones import CalcularResultado
+from funciones import *
 
 import PySimpleGUI as sg
 
@@ -8,7 +8,7 @@ seccion_numeros = [
 
     [sg.Button("",visible = False)],
 
-                                    [sg.Button("0",size = (2,2), key = "0", pad = (39,0,0,0), focus = False)],
+                                    [sg.Button("0",size = (2,2), key = "0", pad = (39,0,0,0))],
 
     [sg.Button("1",size = (2,2),key = "1"), sg.Button("2",size = (2,2),key = "2"), sg.Button("3",size = (2,2),key = "3")],
 
@@ -105,16 +105,22 @@ while True:
     if event in strOperadores and not(EligioOperador):
 
         #Guarda el primer operando, si no se ingresó se supone cero
+        
+        #Si es distinto de vacio ingresó un numero
         if StrNum1 != "":
 
-
+            #Quitar ceros a la izquierda            
             while(StrNum1[0] == "0") and (len(StrNum1) > 1):
                 StrNum1 = StrNum1[1:]
-                    
-                print(StrNum1)
 
+        #Si no ingresó un numero tomar el resultado de la operacion anterior
+        # si la operacion levanto ERROR el primer operando queda en cero
         else:
-            StrNum1 = str(window["-RESULTADO-"].get())
+            
+            Resultado = str(window["-RESULTADO-"].get())
+
+            StrNum1 = Resultado if Resultado != "ERROR" else "0"
+
 
         #Guarda el operador seleccionado
         Op = event
@@ -130,32 +136,35 @@ while True:
     #Si el boton es el igual, resolver el calculo y resetear las variables de string 
     if (event == "="): 
     
-        #Si eligio el operador calcular el resultado
+        #Si eligio el operador significa que selecciono al menos el primer operando
         if (EligioOperador):
      
+            #Si el string no es vacio significa que selecciono el segundo operando
             if StrNum2 != "":
 
                 #Quitar ceros a la izquierda
-
                 while (StrNum2[0] == "0") and (len(StrNum2) > 1):
                     StrNum2 = StrNum2[1:]
 
-                #Num2 = int(StrNum2)
+
+            #Si no selecciono un operando se supone cero
             else:
                 StrNum2 = "0"
                 
+
             #Convertir los strings a flotantes o a enteros segun corresponda
             Num1 = float(StrNum1) if '.' in StrNum1 or 'e' in StrNum1.lower() else int(StrNum1)
             Num2 = float(StrNum2) if '.' in StrNum2 or 'e' in StrNum2.lower() else int(StrNum2)
 
-            #Update del resultado, llama a la funcion que calcula el resultado
+
+            #Update del resultado, llamando a la funcion que calcula el resultado
             window["-RESULTADO-"].update(CalcularResultado(Num1,Num2,Op))
 
             # resetea la variable de control
             EligioOperador = False
 
 
-        #Si no eligio operador es por que solo selecciono un numero, este sera el resultado 
+        #Si no eligio el operador es por que solo selecciono un numero, este sera el resultado 
         # (si solo ingreso 3 y doy a igual, deberia decirme que es = 3)
         else:
 
@@ -182,21 +191,3 @@ while True:
 window.close()    
 
 
-
-""""    
-
-    BUGS A CORREGIR
-
-
-    * AL PRODUCIR UN ERROR EN EL RESULTADO, SI SELECCIONAMOS OTRO OPERADOR EL RESULTADO 'ERROR' SERA EL PRIMER OPERANDO DE ESTA NUEVA CUENTA
-        AL PRESIONAR = NO PUEDE OPERAR CON EL STRING 'ERROR' Y LEVANTA ValueError
-    DE ALGUNA MANERA SETEAR EL OPERANDO A CERO CUANDO ESTE SEA 'ERROR'
-
-
-    * AL OBTENER UN RESULTADO FLOTANTE DEL TIPO 1.0, 1.000, 3.0 ETC, SI SELECCIONAMOS OTRO OPERADOR Y UN OPERANDO ENTERO, EL RESULTADO
-        MANTENDRA ESE DECIMAL NULO, POR EJEMPLO DESDE UN RESULTADO 4.0, SI SELECCIONAMOS LA SUMA Y LUEGO EL NUMERO 2:
-
-        4.0 + 2 = 6.0   EN VEZ DE :     4.0 + 2 = 6
-
-
-"""
--- funciones.py
+++ funciones.py
@@ -2,19 +2,51 @@ def CalcularResultado(num1, num2, op):
     """Funcion que recibe dos numeros reales y un operador aritmetico en
     forma de string, y segun cual es, retorna la cuenta que corresponda """
 
+
     if op == "+":
-        return num1 + num2
+        res = num1 + num2
     
     elif op == "*":
-        return num1 * num2
+
+        res = num1 * num2
     
     elif op == "-":
-        return num1 - num2
+        res = num1 - num2
     
     elif op == "/":
         
+        #Manejar el error de division por cero
         if num2 != 0:
-            return num1 / num2
+            res = num1 / num2
         else:
             return "ERROR"
 
+
+    if '.' in str(res):
+        res = RemoverDecimalNulo(str(res))
+    
+
+
+    return res
+
+
+def RemoverDecimalNulo(num):
+
+    """ 
+        Esta funcion recibe un numero flotante en forma de string y, si tiene un decimal nulo lo elimina y retorna como int,
+            SI EL DECIMAL NO ES NULO LO RETORNA COMO FLOAT
+        por ejemplo:    INPUT: 4.0  OUTPUT: 4       INPUT: 16.000   OUTPUT: 16      INPUT:  50.015     OUTPUT: 50.015
+    """
+    DecimalNulo = True
+
+    #Si algun digito de la parte decimal NO es cero, entonces el decimal no es NULO
+
+    for digito in num.split(".")[1]:
+        if digito != "0":
+            DecimalNulo = False
+
+    #Si el decimal es nulo, quedarse solo con la parte entera
+    if DecimalNulo:
+        return int(num.split(".")[0])
+
+    return float(num)
